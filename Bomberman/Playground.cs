6d7e256deda682c230a8459123f1eb0fe0d05b79
Jan Kubovy <Xsichtik@gmail.com>
2015-07-06 12:30:08 +0200

    Add boraders to Playground

--- Bomberman/Playground.cs
+++ Bomberman/Playground.cs
@@ -20,7 +20,7 @@ namespace Bomberman
 
 	class Playground
 	{
-		private const int playgroundSize = 12; // must be greater then 6!
+		private const int playgroundSize = 14; // must be greater then 8!
 
 		Square[][] board;
 
@@ -35,52 +35,58 @@ namespace Bomberman
 		}
 		private void InitPlayground()
 		{
-			//#region Borders
-
-			//#endregion
+			#region Borders
+			for (int i = 0; i < playgroundSize; i++)
+			{
+				board[0][i] = Square.Unbreakable_Wall;
+				board[playgroundSize - 1][i] = Square.Unbreakable_Wall;
+				board[i][0] = Square.Unbreakable_Wall;
+				board[i][playgroundSize - 1] = Square.Unbreakable_Wall;
+			}
+			#endregion
 			#region Top-left corner
-			board[0][0] = Square.Player_1;
-			board[1][0] = Square.Empty;
-			board[1][1] = Square.Empty;
-			board[0][1] = Square.Empty;
-			board[2][0] = Square.Wall;
-			board[2][1] = Square.Wall;
-			board[1][2] = Square.Wall;
-			board[0][2] = Square.Wall;
-			board[2][2] = Square.Unbreakable_Wall;
+			board[1][1] = Square.Player_1;
+			board[2][1] = Square.Empty;
+			board[2][2] = Square.Empty;
+			board[1][2] = Square.Empty;
+			board[3][1] = Square.Wall;
+			board[3][2] = Square.Wall;
+			board[2][3] = Square.Wall;
+			board[1][3] = Square.Wall;
+			board[3][3] = Square.Unbreakable_Wall;
 			#endregion
 			#region Top-right corner
-			board[0][playgroundSize - 1] = Square.Player_2;
-			board[0][playgroundSize - 2] = Square.Empty;
-			board[1][playgroundSize - 2] = Square.Empty;
-			board[1][playgroundSize - 1] = Square.Empty;
-			board[0][playgroundSize - 3] = Square.Wall;
-			board[1][playgroundSize - 3] = Square.Wall;
-			board[2][playgroundSize - 2] = Square.Wall;
-			board[2][playgroundSize - 1] = Square.Wall;
-			board[2][playgroundSize - 3] = Square.Unbreakable_Wall;
+			board[1][playgroundSize - 2] = Square.Player_2;
+			board[1][playgroundSize - 3] = Square.Empty;
+			board[2][playgroundSize - 3] = Square.Empty;
+			board[2][playgroundSize - 2] = Square.Empty;
+			board[1][playgroundSize - 4] = Square.Wall;
+			board[2][playgroundSize - 4] = Square.Wall;
+			board[3][playgroundSize - 3] = Square.Wall;
+			board[3][playgroundSize - 2] = Square.Wall;
+			board[3][playgroundSize - 4] = Square.Unbreakable_Wall;
 			#endregion
 			#region Bottom-left corner
-			board[playgroundSize - 1][0] = Square.Player_3;
-			board[playgroundSize - 2][0] = Square.Empty;
-			board[playgroundSize - 2][1] = Square.Empty;
-			board[playgroundSize - 1][1] = Square.Empty;
-			board[playgroundSize - 3][0] = Square.Wall;
-			board[playgroundSize - 3][1] = Square.Wall;
-			board[playgroundSize - 2][2] = Square.Wall;
-			board[playgroundSize - 1][2] = Square.Wall;
-			board[playgroundSize - 3][2] = Square.Unbreakable_Wall;
+			board[playgroundSize - 2][1] = Square.Player_3;
+			board[playgroundSize - 3][1] = Square.Empty;
+			board[playgroundSize - 3][2] = Square.Empty;
+			board[playgroundSize - 2][2] = Square.Empty;
+			board[playgroundSize - 4][1] = Square.Wall;
+			board[playgroundSize - 4][2] = Square.Wall;
+			board[playgroundSize - 3][3] = Square.Wall;
+			board[playgroundSize - 2][3] = Square.Wall;
+			board[playgroundSize - 4][3] = Square.Unbreakable_Wall;
 			#endregion
 			#region Bottom-right corner
-			board[playgroundSize - 1][playgroundSize - 1] = Square.Player_4;
-			board[playgroundSize - 2][playgroundSize - 1] = Square.Empty;
-			board[playgroundSize - 2][playgroundSize - 2] = Square.Empty;
-			board[playgroundSize - 1][playgroundSize - 2] = Square.Empty;
-			board[playgroundSize - 3][playgroundSize - 1] = Square.Wall;
-			board[playgroundSize - 3][playgroundSize - 2] = Square.Wall;
-			board[playgroundSize - 2][playgroundSize - 3] = Square.Wall;
-			board[playgroundSize - 1][playgroundSize - 3] = Square.Wall;
-			board[playgroundSize - 3][playgroundSize - 3] = Square.Unbreakable_Wall;
+			board[playgroundSize - 2][playgroundSize - 2] = Square.Player_4;
+			board[playgroundSize - 3][playgroundSize - 2] = Square.Empty;
+			board[playgroundSize - 3][playgroundSize - 3] = Square.Empty;
+			board[playgroundSize - 2][playgroundSize - 3] = Square.Empty;
+			board[playgroundSize - 4][playgroundSize - 2] = Square.Wall;
+			board[playgroundSize - 4][playgroundSize - 3] = Square.Wall;
+			board[playgroundSize - 3][playgroundSize - 4] = Square.Wall;
+			board[playgroundSize - 2][playgroundSize - 4] = Square.Wall;
+			board[playgroundSize - 4][playgroundSize - 4] = Square.Unbreakable_Wall;
 			#endregion
 			do
 			{
@@ -90,25 +96,25 @@ namespace Bomberman
 		private void InitPlaygroundCenter()
 		{
 			Random random = new Random();
-			for (int i = 0; i < 3; i++)
+			for (int i = 1; i < 4; i++)
 			{
-				for (int j = 3; j < (playgroundSize - 3); j++)
+				for (int j = 4; j < playgroundSize - 4; j++)
 				{
 					board[i][j] = (Square)(random.Next(3) + 4);
 					board[j][i] = (Square)(random.Next(3) + 4);
 				}
 			}
-			for (int i = playgroundSize - 3; i < playgroundSize; i++)
+			for (int i = playgroundSize - 4; i < playgroundSize - 1; i++)
 			{
-				for (int j = 3; j < playgroundSize - 3; j++)
+				for (int j = 4; j < playgroundSize - 4; j++)
 				{
 					board[i][j] = (Square)(random.Next(3) + 4);
 					board[j][i] = (Square)(random.Next(3) + 4);
 				}
 			}
-			for (int i = 3; i < playgroundSize - 3; i++)
+			for (int i = 4; i < playgroundSize - 4; i++)
 			{
-				for (int j = 3; j < playgroundSize - 3; j++)
+				for (int j = 4; j < playgroundSize - 4; j++)
 				{
 					board[i][j] = (Square)(random.Next(3) + 4);
 				}
@@ -120,21 +126,21 @@ namespace Bomberman
 		/// <returns></returns>
 		private bool CheckFeasibility()
 		{
-			bool[][] field = new bool[playgroundSize + 2][];
+			bool[][] field = new bool[playgroundSize][];
 			Stack<Point> stack = new Stack<Point>();
 
-			for (int i = 0; i < playgroundSize + 2; i++)
+			for (int i = 0; i < playgroundSize; i++)
 			{
-				field[i] = new bool[playgroundSize + 2];
+				field[i] = new bool[playgroundSize];
 			}
-			for (int i = 1; i < playgroundSize + 1; i++)
+			for (int i = 1; i < playgroundSize - 1; i++)
 			{
-				for (int j = 1; j < playgroundSize + 1; j++)
+				for (int j = 1; j < playgroundSize - 1; j++)
 				{
 					field[i][j] = false;
 				}
 			}
-			for (int i = 0; i < playgroundSize + 2; i++)
+			for (int i = 0; i < playgroundSize; i++)
 			{
 				field[0][i] = true;
 				field[playgroundSize + 1][i] = true;
@@ -146,14 +152,23 @@ namespace Bomberman
 			do
 			{
 				Point p = stack.Pop();
-				if ((field[p.X][p.Y + 1] == false) && (board[p.X - 1][p.Y] != Square.Unbreakable_Wall))
+				if ((field[p.X][p.Y + 1] == false) && (board[p.X][p.Y + 1] != Square.Unbreakable_Wall))
 					stack.Push(new Point(p.X, p.Y + 1)); field[p.X][p.Y + 1] = true;
-				if ((field[p.X + 1][p.Y] == false) && (board[p.X][p.Y - 1] != Square.Unbreakable_Wall))
+				if ((field[p.X + 1][p.Y] == false) && (board[p.X + 1][p.Y] != Square.Unbreakable_Wall))
 					stack.Push(new Point(p.X + 1, p.Y)); field[p.X + 1][p.Y] = true;
-				if ((field[p.X][p.Y - 1] == false) && (board[p.X - 1][p.Y - 2] != Square.Unbreakable_Wall))
+				if ((field[p.X][p.Y - 1] == false) && (board[p.X][p.Y - 1] != Square.Unbreakable_Wall))
 					stack.Push(new Point(p.X, p.Y - 1)); field[p.X][p.Y - 1] = true;
-				if ((field[p.X - 1][p.Y] == false) && (board[p.X - 2][p.Y - 1] != Square.Unbreakable_Wall))
+				if ((field[p.X - 1][p.Y] == false) && (board[p.X - 1][p.Y] != Square.Unbreakable_Wall))
 					stack.Push(new Point(p.X - 1, p.Y)); field[p.X - 1][p.Y] = true;
+
+				//if ((field[p.X][p.Y + 1] == false) && (board[p.X - 1][p.Y] != Square.Unbreakable_Wall))
+				//	stack.Push(new Point(p.X, p.Y + 1)); field[p.X][p.Y + 1] = true;
+				//if ((field[p.X + 1][p.Y] == false) && (board[p.X][p.Y - 1] != Square.Unbreakable_Wall))
+				//	stack.Push(new Point(p.X + 1, p.Y)); field[p.X + 1][p.Y] = true;
+				//if ((field[p.X][p.Y - 1] == false) && (board[p.X - 1][p.Y - 2] != Square.Unbreakable_Wall))
+				//	stack.Push(new Point(p.X, p.Y - 1)); field[p.X][p.Y - 1] = true;
+				//if ((field[p.X - 1][p.Y] == false) && (board[p.X - 2][p.Y - 1] != Square.Unbreakable_Wall))
+				//	stack.Push(new Point(p.X - 1, p.Y)); field[p.X - 1][p.Y] = true;
 			} while (stack.Count != 0);
 			return (field[1][playgroundSize] & field[playgroundSize][1] & field[playgroundSize][playgroundSize]);
 		}
